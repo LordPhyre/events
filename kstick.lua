bcffe4085ea59919678f9ac26ac7dd33f7ce83a6
LordPhyre <60118488+LordPhyre@users.noreply.github.com>
2021-04-23 16:39:51 -0400

    Update kstick.lua

--- kstick.lua
+++ kstick.lua
@@ -1,8 +1,8 @@
 local stick_knockback = 25 --how hard the stick smacks kids. recommended min: 20
 local stick_pointing_distance = 5 --selecting distance of smacking
-admintools_invincible = {}
-admintools_timeouts = {}
-admintools_jumpouts = {}
+events_invincible = {}
+events_timeouts = {}
+events_jumpouts = {}
 
 
 minetest.register_craftitem("events:knockbackstick", {
@@ -17,7 +17,7 @@ minetest.register_craftitem("events:knockbackstick", {
     on_use = function(itemstack, user, pointed_thing)
 
         local p_name = user:get_player_name()
-        local last_push_time = admintools_timeouts[p_name] or 0.0
+        local last_push_time = events_timeouts[p_name] or 0.0
         local current_time = minetest.get_us_time()/1000000.0
         local time_from_last_push = current_time-last_push_time
         local force = stick_knockback
@@ -39,9 +39,9 @@ minetest.register_craftitem("events:knockbackstick", {
                 if vector.distance(hitted_pos,hitter_pos) < stick_pointing_distance then
 
                     local pointed_name = pointed_thing.ref:get_player_name()
-                    if not admintools_invincible[pointed_name] then
+                    if not events_invincible[pointed_name] then
                         pointed_thing.ref:add_player_velocity(vector.multiply(dir, force))
-                        admintools_timeouts[p_name] = current_time
+                        events_timeouts[p_name] = current_time
 
                     end
                     
