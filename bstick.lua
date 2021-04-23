7a34a81bb95e00431e2bc833f7e9d0f5811a7015
LordPhyre <60118488+LordPhyre@users.noreply.github.com>
2021-04-23 16:36:03 -0400

    Update bstick.lua

--- bstick.lua
+++ bstick.lua
@@ -1,8 +1,8 @@
 local stick_knockback = 40 --how hard the stick smacks kids. recommended min: 35
 local stick_bounce_reach = 25 --selecting distance of bouncing
 local stick_bounce_timeout = 0.1 --bounce limiter in seconds. recommended min: 0.1
-events.timeouts = {}
-events.jumpouts = {}
+events_timeouts = {}
+events_jumpouts = {}
 
 
 minetest.register_craftitem("events:broomstick", {
@@ -16,7 +16,7 @@ minetest.register_craftitem("events:broomstick", {
     on_place = function(itemstack, placer, pointed_thing)
 
         local p_name = placer:get_player_name()
-        local last_jump_time = events.jumpouts[p_name] or 0.0
+        local last_jump_time = events_jumpouts[p_name] or 0.0
         local current_time = minetest.get_us_time()/1000000.0 --microsec converted to sec
         local time_from_last_jump = current_time-last_jump_time
         
@@ -28,7 +28,7 @@ minetest.register_craftitem("events:broomstick", {
                     local pushvect = vector.normalize( {x=lookvect.x, z=lookvect.z, y= math.sqrt(1-(lookvect.y*lookvect.y))})
                     --gives a unit vector that is 90 deg offset in the vert direction
                     local force = 8 * vector.length(vector.normalize( {x=lookvect.x, z=lookvect.z, y= 0}))
-                    events.jumpouts[p_name] = current_time
+                    events_jumpouts[p_name] = current_time
                     placer:add_player_velocity(vector.multiply(pushvect, force))
                 end
             end
